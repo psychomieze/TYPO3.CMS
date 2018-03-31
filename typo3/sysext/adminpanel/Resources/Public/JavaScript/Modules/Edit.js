06422becbe45680290bb3ad154de59414c2f1c72
Susanne Moog <susanne.moog@typo3.org>
2018-03-31 16:41:36 +0200

    simulate date js
    Change-Id: Id12e5ca9901ffa250ead16cef4e02f34f621527c

--- typo3/sysext/adminpanel/Resources/Public/JavaScript/Modules/Edit.js
+++ typo3/sysext/adminpanel/Resources/Public/JavaScript/Modules/Edit.js
@@ -1,32 +1,24 @@
 function editModuleOnClickHandler(event) {
-  event.preventDefault();
-  var element = event.target;
-  if (parent.opener && parent.opener.top) {
-    parent.opener.top.fsMod.recentIds['web'] = element.getAttribute('data-pageUid');
-    if (parent.opener.top && parent.opener.top.nav_frame && parent.opener.top.nav_frame.refresh_nav) {
-      parent.opener.top.nav_frame.refresh_nav();
-    }
-    parent.opener.top.goToModule(element.getAttribute('data-pageModule'));
-    parent.opener.top.focus();
-  } else {
-    const vHWin = window.open(element.getAttribute('data-backendScript'), element.getAttribute('data-t3BeSitenameMd5'));
-    vHWin.focus();
-  }
-  return false;
+	event.preventDefault();
+	var element = event.target;
+	if (parent.opener && parent.opener.top) {
+		parent.opener.top.fsMod.recentIds['web'] = element.getAttribute('data-pageUid');
+		if (parent.opener.top && parent.opener.top.nav_frame && parent.opener.top.nav_frame.refresh_nav) {
+			parent.opener.top.nav_frame.refresh_nav();
+		}
+		parent.opener.top.goToModule(element.getAttribute('data-pageModule'));
+		parent.opener.top.focus();
+	} else {
+		const vHWin = window.open(element.getAttribute('data-backendScript'), element.getAttribute('data-t3BeSitenameMd5'));
+		vHWin.focus();
+	}
+	return false;
 }
 
 function initializeEditModule() {
-  var editModuleBtnOpenBackend = document.querySelector('.typo3-adminPanel-btn-openBackend');
-  if (editModuleBtnOpenBackend.addEventListener) {
-    editModuleBtnOpenBackend.addEventListener('click', editModuleOnClickHandler);
-  } else if (editModuleBtnOpenBackend.attachEvent) {
-    editModuleBtnOpenBackend.addEventListener('click', editModuleOnClickHandler);
-  }
+	var editModuleBtnOpenBackend = document.querySelector('.typo3-adminPanel-btn-openBackend');
+	editModuleBtnOpenBackend.addEventListener('click', editModuleOnClickHandler);
 }
 
 
-if (window.addEventListener) {
-  window.addEventListener('load', initializeEditModule, false);
-} else if (window.attachEvent) {
-  window.attachEvent('onload', initializeEditModule);
-}
+window.addEventListener('load', initializeEditModule, false);
